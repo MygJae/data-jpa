75befaf035b89817277b0511b9a6b4ff9c780a99
Mygjae <50861514+MygJae@users.noreply.github.com>
2023-10-22 23:50:32 +0900

    쿼리 메소드 기능3

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,13 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import java.awt.print.Pageable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -34,4 +36,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findMemberByUsernames(String username);//단건
     Optional<Member> findOptionalByUsernames(String username);//단건 Optional
 
+    @Query(value = "select m from Member m left join m.team",
+            countQuery = "select count(m.username) from Member m")
+    Page<Member> findByAge(int age, Pageable pageable);
 }
